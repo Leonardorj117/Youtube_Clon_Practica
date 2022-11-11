9bc38a9e370e7f975108b4f274fb35fa9e991edd
Leonardo <Leonardorj12393@gmail.com>
2022-11-11 13:26:54 -0600

    assets agregados

---
+++ Youtube_Clon_Practica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube_Clon_Practica
+//
+//  Created by Leonardo Rubio on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
